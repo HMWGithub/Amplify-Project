176d79f26d47c551a1511e032e9de7914e53ddac
Harrison Max-Wilson <H.MaxWilson@hotmail.com>
2022-04-02 01:05:42 +0100

    Tidied up alerts and remembering form values

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -26,7 +26,7 @@
     private function validateUsername($un) {
       // Check username length
       if (strlen($un) > 25 || strlen($un) < 5){
-        array_push($this->errorArray, "Your username must be between 5 and 25 characters");
+        array_push($this->errorArray, Constants::$usernameInvalidLength);
         return;
       }
 
@@ -36,25 +36,25 @@
 
     private function validateFirstName($fn) {
       if (strlen($fn) > 25 || strlen($fn) < 2){
-        array_push($this->errorArray, "Your first name must be between 2 and 25 characters");
+        array_push($this->errorArray, Constants::$firstNameInvalidLength);
         return;
       }
     }
 
     private function validateLastName($ln) {
       if (strlen($ln) > 25 || strlen($ln) < 2){
-        array_push($this->errorArray, "Your last name must be between 2 and 25 characters");
+        array_push($this->errorArray, Constants::$lastNameInvalidLength);
         return;
       }
     }
 
     private function validateEmail($em, $em2) {
       if ($em != $em2){
-        array_push($this->errorArray, "Your emails do not match");
+        array_push($this->errorArray, Constants::$emailsDoNotMatch);
         return;
       }
       if (!filter_var($em, FILTER_VALIDATE_EMAIL)){
-        array_push($this->errorArray, "Your email is invalid");
+        array_push($this->errorArray, Constants::$emailInvalidFormat);
         return;
       }
       
@@ -63,15 +63,15 @@
 
     private function validatePassword($pw, $pw2) {
       if($pw != $pw2){
-        array_push($this->errorArray, "Your passwords do not match");
+        array_push($this->errorArray, Constants::$passwordsDoNotMatch);
         return;
       }
       if (preg_match('/[^A-Za-z0-9]/', $pw)){
-        array_push($this->errorArray, "Your password can only contain numbers and letters");
+        array_push($this->errorArray, Constants::$passwordNotAlphanumeric);
         return;
       }
       if (strlen($pw) > 30 || strlen($pw) < 8){
-        array_push($this->errorArray, "Your password must be between 8 and 30 characters");
+        array_push($this->errorArray, Constants::$passwordInvalidLength);
         return;
       }
     }
--- register.php
+++ register.php
@@ -1,7 +1,14 @@
 <?php 
-  require_once("includes/classes/Account.php");
+  include("includes/classes/Account.php");
+  include("includes/classes/Constants.php");
 
   $account = new Account();
+
+  function getInputValue($name){
+    if(isset($_POST[$name])){
+      echo $_POST[$name];
+    }
+  }
 
 ?>
 <?php include("includes/handlers/register-handler.php"); ?>
@@ -37,40 +44,40 @@
     <form id="registerForm" action="register.php" method="POST">
       <h2>Create your free account</h2>
       <p>
-        <p><?php echo $account->getError("Your username must be between 5 and 25 characters"); ?></p>
+        <p><?php echo $account->getError(Constants::$usernameInvalidLength); ?></p>
         <label for="registerUsername">Username</label>
-        <input id="registerUsername" name="registerUsername" type="text" placeholder="e.g. beatifyUser123" required>
+        <input id="registerUsername" name="registerUsername" type="text" placeholder="e.g. beatifyUser123" value="<?php getInputValue('registerUsername')?>" required>
       </p>
       <p>
-        <p><?php echo $account->getError("Your first name must be between 2 and 25 characters"); ?></p>
+        <p><?php echo $account->getError(Constants::$firstNameInvalidLength); ?></p>
         <label for="registerFirstname">Firstname</label>
-        <input id="registerFirstname" name="registerFirstname" type="text" placeholder="e.g. John" required>
+        <input id="registerFirstname" name="registerFirstname" type="text" placeholder="e.g. John" value="<?php getInputValue('registerFirstname')?>" required>
       </p>
       <p>
-        <p><?php echo $account->getError("Your last name must be between 2 and 25 characters"); ?></p>
+        <p><?php echo $account->getError(Constants::$lastNameInvalidLength); ?></p>
         <label for="registerLastname">Lastname</label>
-        <input id="registerLastname" name="registerLastname" type="text" placeholder="e.g. Smith" required>
+        <input id="registerLastname" name="registerLastname" type="text" placeholder="e.g. Smith" value="<?php getInputValue('registerLastname')?>" required>
       </p>
       <p>
-        <p><?php echo $account->getError("Your emails do not match"); ?></p>
-        <p><?php echo $account->getError("Your email is invalid"); ?></p>
+        <p><?php echo $account->getError(Constants::$emailsDoNotMatch); ?></p>
+        <p><?php echo $account->getError(Constants::$emailInvalidFormat); ?></p>
         <label for="registerEmail">Email</label>
-        <input id="registerEmail" name="registerEmail" type="email" placeholder="e.g. john@gmail.com" required>
+        <input id="registerEmail" name="registerEmail" type="email" placeholder="e.g. john@gmail.com" value="<?php echo getInputValue('registerEmail')?>" required>
       </p>
       <p>
         <label for="registerEmail2">Confirm Email</label>
-        <input id="registerEmail2" name="registerEmail2" type="email" placeholder="e.g. john@gmail.com" required>
+        <input id="registerEmail2" name="registerEmail2" type="email" placeholder="e.g. john@gmail.com" value="<?php echo getInputValue('registerEmail2')?>" required>
       </p>
       <p>
-        <p><?php echo $account->getError("Your passwords do not match"); ?></p>
-        <p><?php echo $account->getError("Your password can only contain numbers and letters"); ?></p>
-        <p><?php echo $account->getError("Your password must be between 8 and 30 characters"); ?></p>
+        <p><?php echo $account->getError(Constants::$passwordsDoNotMatch); ?></p>
+        <p><?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?></p>
+        <p><?php echo $account->getError(Constants::$passwordInvalidLength); ?></p>
         <label for="registerPassword">Password</label>
-        <input id="registerPassword" name="registerPassword" type="password" placeholder="Your password" required>
+        <input id="registerPassword" name="registerPassword" type="password" placeholder="password" required>
       </p>
       <p>
         <label for="registerPassword2">Confirm Password</label>
-        <input id="registerPassword2" name="registerPassword2" type="password" placeholder="Your password" required>
+        <input id="registerPassword2" name="registerPassword2" type="password" placeholder="password" required>
       </p>
 
       <button type="submit" name="register">SIGN UP</button>
