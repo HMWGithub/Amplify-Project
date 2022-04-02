547878ced8a65a82d3139c0d29f9c82c80b19ba6
Harrison Max-Wilson <H.MaxWilson@hotmail.com>
2022-04-02 01:18:36 +0100

    adding database connection

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+  ob_start();
+
+  $timezone = date_default_timezone_set("Europe/London");
+  $connection = mysqli_connect("localhost", "root", "", "Beatify");
+
+  if(mysqli_connect_errno()){
+    echo "Failed to connect: " . $mysqli_connect_errno();
+  }
+?>
